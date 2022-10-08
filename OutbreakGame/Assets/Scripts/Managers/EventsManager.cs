15b37ccd80944dd0ffd74b7b6234349a768d05de
lucas <lugomez@itba.edu.ar>
2022-10-08 19:37:24 -0300

    Changes to EventsManager

--- OutbreakGame/Assets/Scripts/Managers/EventsManager.cs
+++ OutbreakGame/Assets/Scripts/Managers/EventsManager.cs
@@ -12,8 +12,19 @@ public class EventsManager : MonoBehaviour
         instance = this;
     }
 
+    #region GAME_OVER
     public event Action<bool> OnGameOver;
     public void EventGameOver(bool isVictory){
         if(OnGameOver != null) OnGameOver(isVictory);
     }
+    #endregion
+
+    #region UI_EVENTS
+    public event Action<int, int> OnAmmoChange;
+    public void AmmoChange(int currentAmmo, int maxAmmo){
+        if(OnAmmoChange != null) OnAmmoChange(currentAmmo, maxAmmo);    //TODO: Crean AmmoChangeActivator en Gun.cs
+    }
+    #endregion
+
+
 }
