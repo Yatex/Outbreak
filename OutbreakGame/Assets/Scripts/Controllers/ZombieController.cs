4fd249c7e2dfd6fbdacabe599a82a7d61bed09b1
gbarbieripederiva <gbarbieripederiva@gmail.com>
2022-10-10 23:20:53 -0300

    Fix zombie controller

--- OutbreakGame/Assets/Scripts/Controllers/ZombieController.cs
+++ OutbreakGame/Assets/Scripts/Controllers/ZombieController.cs
@@ -6,11 +6,17 @@ public class ZombieController : MonoBehaviour, IMoveable
     public float Speed => GetComponent<Actor>().ActorStats.MovementSpeed;
     public float RotationSpeed => GetComponent<Actor>().ActorStats.RotationSpeed;
     
-    public void Travel(Vector3 target) => transform.position = Vector3.MoveTowards(
+    public void Travel(Vector3 target) => MoveAtSpeed(target, Speed);
+    public void Sprint(Vector3 target) => MoveAtSpeed(target, Speed * 1.5f);
+
+    public void Rotate(Vector3 target) => transform.LookAt(target);
+
+    #region PrivateMethods
+    private void MoveAtSpeed(Vector3 target, float Speed) => transform.position = Vector3.MoveTowards(
             transform.position,
             target,
             Time.deltaTime * Speed
         );
+    #endregion
 
-    public void Rotate(Vector3 target) => transform.LookAt(target);
 }
