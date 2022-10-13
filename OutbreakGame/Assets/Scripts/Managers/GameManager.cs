e15e8a2829ebd5c30e1120cd4bea5dfb8b321da6
Lucas <lugomez@itba.edu.ar>
2022-10-13 17:13:13 -0300

    Fixed unity version

--- OutbreakGame/Assets/Scripts/Managers/GameManager.cs
+++ OutbreakGame/Assets/Scripts/Managers/GameManager.cs
@@ -1,26 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
     [SerializeField] private bool _isGameOver = false;
-    [SerializeField] private bool _isVictory = false;
-    [SerializeField] private Text _gameoverMessage;
 
     void Start()
     {
         EventsManager.instance.OnGameOver += OnGameOver;
-        _gameoverMessage.text = string.Empty;
     }
 
     private void OnGameOver(bool isVictory){
         _isGameOver = true;
-        _isVictory = isVictory;
-
-        _gameoverMessage.text = isVictory ? "VICTORIA" : "DERROTA";
-        _gameoverMessage.color = isVictory? Color.green : Color.red;
+        Cursor.lockState = CursorLockMode.None;
+        Invoke("LoadEndGameScene", 3);
     }
+    private void LoadEndGameScene() => SceneManager.LoadScene("EndGame");
 }
