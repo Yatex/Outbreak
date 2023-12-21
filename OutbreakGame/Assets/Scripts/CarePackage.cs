487a578c2d3c2562cf6bf7be76fc42f108497795
Lucas Gomez <lugomez@itba.edu.ar>
2023-12-21 18:30:54 -0300

    Fix to care package

--- OutbreakGame/Assets/Scripts/CarePackage.cs
+++ OutbreakGame/Assets/Scripts/CarePackage.cs
@@ -11,12 +11,6 @@ public class CarePackage : MonoBehaviour
     {
         // Set the initial downward velocity to control the fall speed
         Rigidbody rb = GetComponent<Rigidbody>();
-        if (rb != null)
-        {
-            rb.drag = fallSpeed;
-            rb.isKinematic = false; // Make the care package static
-            rb.useGravity = true;
-        }
     }
 
     void Update()
@@ -25,46 +19,19 @@ public class CarePackage : MonoBehaviour
         //transform.Translate(Vector3.down * descentSpeed * Time.deltaTime);
     }
 
-    void OnTriggerEnter(Collider other)
+    
+
+    void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Care package collided with " + other.name);
+        Debug.Log("Care package collided with " + collision.gameObject.name);
         // Check if the care package collides with the player
-        if (other.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             // Restore ammo and destroy the care package
             _currentGun.Ammo = ammoRestoreAmount;
             _currentGun.UI_AmmoUpdater();
 
             Destroy(gameObject);
-        }
-        else if (other.CompareTag("Ground"))
-        {
-            // Stop further movement or take any other necessary action
-            Rigidbody rb = GetComponent<Rigidbody>();
-            if (rb != null)
-            {
-                rb.velocity = Vector3.zero; // Stop the care package from moving
-                rb.isKinematic = true; // Make the care package static
-                rb.useGravity = false;
-                transform.position += new Vector3(0f, 0.15f, 0f);
-                
-            }
-        }
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Care package collided with " + collision.gameObject.name);
-        // Check if the care package collides with the ground
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            // Stop further movement or take any other necessary action
-            Rigidbody rb = GetComponent<Rigidbody>();
-            if (rb != null)
-            {
-                rb.velocity = Vector3.zero; // Stop the care package from moving
-                rb.isKinematic = true; // Make the care package static
-            }
         }
     }
 }
